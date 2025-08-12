e8148dbe22360d8dc2814c1064e28d38adbb2364
Karthikeya-15 <karthikmr.kk200515@gmail.com>
2025-08-12 14:31:05 +0530

    Add files via upload

---
+++ class ex1.java
@@ -0,0 +1,9 @@
+class classex1
+{
+public static void main(String args[])
+{
+    int x=5,y=7;
+    int c=x+y;
+System.out.println("add is "+c);
+}
+}
---
+++ teast.java
@@ -0,0 +1,12 @@
+class Test{
+public static void main(String args[])
+{
+int x=5,y=7,c;
+c=x+y;
+System.out.println("addition is"+c);
+for(;c<50;c++)
+{
+System.out.println("addition is"+c);}
+
+}
+}
